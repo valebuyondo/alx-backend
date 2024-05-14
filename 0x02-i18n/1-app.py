651289583014fe9c2476fa0e805cc21618039adf
Buyondo Vale <42058151+valebuyondo@users.noreply.github.com>
2024-05-14 16:49:43 +0300

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,25 +6,22 @@ from flask import Flask, render_template
 
 
 class Config:
-        """Represents a Flask Babel configuration.
-            """
-                LANGUAGES = ["en", "fr"]
-                    BABEL_DEFAULT_LOCALE = "en"
-                        BABEL_DEFAULT_TIMEZONE = "UTC"
+"""Represents a Flask Babel configuration."""
+LANGUAGES = ["en", "fr"]
+BABEL_DEFAULT_LOCALE = "en"
+BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
-                        app = Flask(__name__)
-                        app.config.from_object(Config)
-                        app.url_map.strict_slashes = False
-                        babel = Babel(app)
+ @app.route('/')
+def get_index() -> str:
+        """The home/index page."""
+        return render_template('1-index.html')
 
 
-                        @app.route('/')
-                        def get_index() -> str:
-                                """The home/index page.
-                                    """
-                                        return render_template('1-index.html')
-
-
-                                    if __name__ == '__main__':
-                                            app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
