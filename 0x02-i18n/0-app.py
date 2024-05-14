7dbe87a51e3dc14ab051b83bbcc7bffdf6cba445
Buyondo Vale <42058151+valebuyondo@users.noreply.github.com>
2024-05-14 16:40:14 +0300

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,10 +10,10 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def get_index() -> str:
-    """The home/index page."""
+    """The home/index page.
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
